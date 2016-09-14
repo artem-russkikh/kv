cd44482c212e2fcd7959e7c93fa0121deb963c1a
Artem Russkikh <rusartx@gmail.com>
2016-09-14 06:29:16 +0500

    Separate test

--- test/kv/bucket_test.exs
+++ test/kv/bucket_test.exs
@@ -6,12 +6,18 @@ defmodule KV.BucketTest do
     {:ok, bucket: bucket}
   end
 
+  test "deletes values by key", %{bucket: bucket} do
+    assert KV.Bucket.get(bucket, "milk") == nil
+
+    KV.Bucket.put(bucket, "milk", 3)
+    assert KV.Bucket.delete(bucket, "milk") == 3
+    assert KV.Bucket.get(bucket, "milk") == nil
+  end
+
   test "stores values by key", %{bucket: bucket} do
     assert KV.Bucket.get(bucket, "milk") == nil
 
     KV.Bucket.put(bucket, "milk", 3)
     assert KV.Bucket.get(bucket, "milk") == 3
-    assert KV.Bucket.delete(bucket, "milk") == 3
-    assert KV.Bucket.get(bucket, "milk") == nil
   end
 end
