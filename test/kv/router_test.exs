913373d856bdba798c292cecf08353dd120f8fef
Artem Russkikh <rusartx@gmail.com>
2016-09-16 17:42:25 +0500

    Router test passing

---
+++ test/kv/router_test.exs
@@ -0,0 +1,16 @@
+defmodule KV.RouterTest do
+  use ExUnit.Case, async: true
+
+  test "route requests across nodes" do
+    assert KV.Router.route("hello", Kernel, :node, []) ==
+           :"foo@MBP-Artem"
+    assert KV.Router.route("world", Kernel, :node, []) ==
+           :"bar@MBP-Artem"
+  end
+
+  test "raises on unknown entries" do
+    assert_raise RuntimeError, ~r/could not find entry/, fn ->
+      KV.Router.route(<<0>>, Kernel, :node, [])
+    end
+  end
+end
