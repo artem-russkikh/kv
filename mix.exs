3c4af8184a1bee69311f9bafa6bc498bd2d67a07
Artem Russkikh <rusartx@gmail.com>
2016-09-16 15:07:24 +0500

    Move to umbrella project

--- mix.exs
+++ mix.exs
@@ -5,6 +5,10 @@ defmodule KV.Mixfile do
     [app: :kv,
      version: "0.1.0",
      elixir: "~> 1.3",
+     build_path: "../../_build",
+     config_path: "../../config/config.exs",
+     deps_path: "../../deps",
+     lockfile: "../../mix.lock",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps()]
