2bdcea193868f1e8c994f60a2fb74789fc8ae8d8
Artem Russkikh <rusartx@gmail.com>
2016-09-16 17:37:31 +0500

    Use Task.Supervisor

--- lib/kv/supervisor.ex
+++ lib/kv/supervisor.ex
@@ -8,7 +8,7 @@ defmodule KV.Supervisor do
   def init(:ok) do
     children = [
       worker(KV.Registry, [KV.Registry]),
-      supervisor(KV.Bucket.Supervisor, [])
+      supervisor(Task.Supervisor, [[name: KV.RouterTasks]])
     ]
 
     supervise(children, strategy: :rest_for_one)
